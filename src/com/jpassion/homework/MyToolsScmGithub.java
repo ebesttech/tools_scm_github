6ca6fbf81494015f0dcde454991ce0b24305e952
Horace Ho <horaceho@ebesttech.com>
2014-09-09 16:53:01 -0400

    Initial commit

---
+++ src/com/jpassion/homework/MyToolsScmGithub.java
@@ -0,0 +1,14 @@
+package com.jpassion.homework;
+
+public class MyToolsScmGithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new");
+
+	}
+
+}
